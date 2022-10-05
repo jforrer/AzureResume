165c58b225bc7bbbbef7b6791b4be1a755c62195
Justin Forrer <justinforrer@gmail.com>
2022-10-04 20:32:44 -0500

    updates frontend main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,13 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = "https://getforrerresumecounter.azurewebsites.net/api/GetResumeCounter?code=pE3zPEK8ACPGJ1ec3Enquy_5UuIe1-sRfReiZd_ZO4XUAzFuCC1Fow=="
+
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
